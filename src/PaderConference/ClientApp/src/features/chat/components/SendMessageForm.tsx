62c4842f7eb3a0b6e6617211407681313a0e050b
Vincent (Anapher) <anapher@protonmail.com>
2020-10-26 21:50:01 +0100

    add emojis to chat

--- src/PaderConference/ClientApp/src/features/chat/components/SendMessageForm.tsx
+++ src/PaderConference/ClientApp/src/features/chat/components/SendMessageForm.tsx
@@ -1,14 +1,32 @@
-import { Box, IconButton, TextField, Typography } from '@material-ui/core';
-import React from 'react';
+import { Box, ClickAwayListener, Grow, IconButton, Paper, Popper, TextField, Typography } from '@material-ui/core';
+import React, { useRef, useState } from 'react';
 import SendIcon from '@material-ui/icons/Send';
 import { useForm } from 'react-hook-form';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import EmojisPopper from './EmojisPopper';
 
 type Props = {
    onSendMessage: (msg: string) => void;
 };
 
 export default function SendMessageForm({ onSendMessage }: Props) {
-   const { register, handleSubmit, reset, formState } = useForm({ mode: 'onChange' });
+   const { register, handleSubmit, reset, setValue, watch } = useForm({ mode: 'onChange' });
+
+   const message = watch('message');
+
+   const [emojisPopperOpen, setEmojisPopperOpen] = useState(false);
+   const emojisButtonRef = useRef(null);
+
+   const handleCloseEmojis = () => setEmojisPopperOpen(false);
+   const handleOpenEmojis = () => setEmojisPopperOpen(true);
+
+   const inputRef = useRef<any | null>(null);
+
+   const handleInsertEmoji = (s: string) => {
+      setValue('message', message + s);
+      handleCloseEmojis();
+      inputRef.current?.focus();
+   };
 
    return (
       <form
@@ -22,15 +40,37 @@ export default function SendMessageForm({ onSendMessage }: Props) {
             placeholder="Type your message..."
             autoComplete="off"
             fullWidth
-            inputRef={register({ required: true })}
+            inputRef={(ref) => {
+               register(ref);
+               inputRef.current = ref;
+            }}
             name="message"
          />
          <Box display="flex" flexDirection="row" justifyContent="space-between" alignItems="center">
             <Typography>Vincent</Typography>
-            <IconButton aria-label="send" type="submit" disabled={!formState.isValid || !formState.isDirty}>
-               <SendIcon fontSize="small" />
-            </IconButton>
+            <Box display="flex">
+               <IconButton aria-label="emojis" ref={emojisButtonRef} onClick={handleOpenEmojis}>
+                  <EmojiEmotionsIcon fontSize="small" />
+               </IconButton>
+               <IconButton aria-label="send" type="submit" disabled={!message}>
+                  <SendIcon fontSize="small" />
+               </IconButton>
+            </Box>
          </Box>
+
+         <Popper open={emojisPopperOpen} anchorEl={emojisButtonRef.current} transition placement="top-end">
+            {({ TransitionProps }) => (
+               <Grow {...TransitionProps}>
+                  <Paper>
+                     <ClickAwayListener onClickAway={handleCloseEmojis}>
+                        <Box p={1}>
+                           <EmojisPopper onClose={handleCloseEmojis} onEmojiSelected={handleInsertEmoji} />
+                        </Box>
+                     </ClickAwayListener>
+                  </Paper>
+               </Grow>
+            )}
+         </Popper>
       </form>
    );
 }
