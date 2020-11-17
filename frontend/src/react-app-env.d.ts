51ea83bf20c49626fac33c0204b7625c5ef5ff59
Vincent (Anapher) <anapher@protonmail.com>
2020-11-17 21:30:46 +0100

    fix emoji regex package

--- frontend/src/react-app-env.d.ts
+++ frontend/src/react-app-env.d.ts
@@ -4,3 +4,9 @@ declare module '*.mp3' {
    const src: string;
    export default src;
 }
+
+declare module 'emoji-regex/RGI_Emoji' {
+   function emojiRegex(): RegExp;
+
+   export = emojiRegex;
+}
