bd0c2079397e516d30be179b1d1e488ca31d14f3
Vincent (Anapher) <anapher@protonmail.com>
2020-12-14 21:39:51 +0100

    comment integration test

--- test/PaderConference.IntegrationTests/Controllers/CreateConferenceTest.cs
+++ test/PaderConference.IntegrationTests/Controllers/CreateConferenceTest.cs
@@ -1,43 +1,44 @@
-﻿using System.Collections.Immutable;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
-using PaderConference.IntegrationTests._Helpers;
-using PaderConference.Models.Request;
-using PaderConference.Models.Response;
-using Xunit;
+﻿//using System.Collections.Immutable;
+//using System.Net;
+//using System.Net.Http;
+//using System.Net.Http.Headers;
+//using System.Threading.Tasks;
+//using PaderConference.IntegrationTests._Helpers;
+//using PaderConference.Models.Request;
+//using PaderConference.Models.Response;
+//using Xunit;
 
-namespace PaderConference.IntegrationTests.Controllers
-{
-    public class CreateConferenceTest : IClassFixture<CustomWebApplicationFactory>
-    {
-        private readonly HttpClient _client;
+//namespace PaderConference.IntegrationTests.Controllers
+//{
+//    public class CreateConferenceTest : IClassFixture<CustomWebApplicationFactory>
+//    {
+//        private readonly HttpClient _client;
 
-        public CreateConferenceTest(CustomWebApplicationFactory factory)
-        {
-            _client = factory.CreateClient();
-        }
+//        public CreateConferenceTest(CustomWebApplicationFactory factory)
+//        {
+//            _client = factory.CreateClient();
+//        }
 
-        [Fact]
-        public async Task Run()
-        {
-            var response = await _client.PostAsync("/api/v1/auth/login",
-                new JsonContent(new LoginRequestDto {UserName = "Vincent", Password = "123"}));
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//        [Fact]
+//        public async Task Run()
+//        {
+//            var response = await _client.PostAsync("/api/v1/auth/login",
+//                new JsonContent(new LoginRequestDto {UserName = "Vincent", Password = "123"}));
+//            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            var result = await response.Content.DeserializeJsonObject<LoginResponseDto>();
-            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
+//            var result = await response.Content.DeserializeJsonObject<LoginResponseDto>();
+//            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", result.AccessToken);
 
-            response = await _client.PostAsync("/api/v1/conference",
-                new JsonContent(new CreateConferenceRequestDto
-                    {ConferenceType = "class", Moderators = new[] {"test"}.ToImmutableList(), Name = "Test con"}));
+//            response = await _client.PostAsync("/api/v1/conference",
+//                new JsonContent(new CreateConferenceRequestDto
+//                    {ConferenceType = "class", Moderators = new[] {"test"}.ToImmutableList(), Name = "Test con"}));
 
-            var asd = await response.Content.ReadAsStringAsync();
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+//            var asd = await response.Content.ReadAsStringAsync();
+//            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            var conferenceInfo = await response.Content.DeserializeJsonObject<StartConferenceResponseDto>();
-            Assert.NotEmpty(conferenceInfo.ConferenceId);
-        }
-    }
-}
+//            var conferenceInfo = await response.Content.DeserializeJsonObject<StartConferenceResponseDto>();
+//            Assert.NotEmpty(conferenceInfo.ConferenceId);
+//        }
+//    }
+//}
+
--- test/PaderConference.IntegrationTests/CustomWebApplicationFactory.cs
+++ test/PaderConference.IntegrationTests/CustomWebApplicationFactory.cs
@@ -1,22 +1,23 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Extensions.DependencyInjection;
-using PaderConference.Core.Interfaces.Gateways.Repositories;
-using PaderConference.IntegrationTests.Mock;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Mvc.Testing;
+//using Microsoft.Extensions.DependencyInjection;
+//using PaderConference.Core.Interfaces.Gateways.Repositories;
+//using PaderConference.IntegrationTests.Mock;
 
-namespace PaderConference.IntegrationTests
-{
-    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
-    {
-        protected override void ConfigureWebHost(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices(services =>
-            {
-                services.AddSingleton<IConferenceRepo, ConferenceRepoMock>();
-                //var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IDocumentStore));
-                //if (descriptor != null)
-                //    services.Remove(descriptor);
-            });
-        }
-    }
-}
+//namespace PaderConference.IntegrationTests
+//{
+//    public class CustomWebApplicationFactory : WebApplicationFactory<Startup>
+//    {
+//        protected override void ConfigureWebHost(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureServices(services =>
+//            {
+//                services.AddSingleton<IConferenceRepo, ConferenceRepoMock>();
+//                //var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IDocumentStore));
+//                //if (descriptor != null)
+//                //    services.Remove(descriptor);
+//            });
+//        }
+//    }
+//}
+
