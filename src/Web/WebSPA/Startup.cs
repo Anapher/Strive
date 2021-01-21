9cb307443bdd3b4b059da14b06a04cf0b0fb6129
Vincent (Anapher) <anapher@protonmail.com>
2021-01-21 12:30:58 +0100

    add SPA as microservice

---
+++ src/Web/WebSPA/Startup.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+
+namespace WebSPA
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks().AddCheck("self", () => HealthCheckResult.Healthy());
+
+            services.AddControllers();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(configuration => { configuration.RootPath = "Client/build"; });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+            app.UseSpaStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/health");
+            });
+
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "Client";
+
+                if (env.IsDevelopment())
+                    // uncomment this if you want the React app to start with ASP.Net Core (else you have to start it manually)
+                    spa.UseReactDevelopmentServer("start");
+                //spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+            });
+        }
+    }
+}
---
+++ src/Web/WebSPA/index.md
@@ -0,0 +1,4 @@
+# WebSPA based on React
+
+## Development
+The ASP.Net Core Project is just a shell around the React App. You can develop the React app as usual using your favorite editor (I use VSCode) in the `/Client` directory. To debug the React app, you may just start the ASP.Net Core project, in Debug mode it will serve the React app using the `start` script (so hot reload etc. will be available).
