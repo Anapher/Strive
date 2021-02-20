627422861f2fc7597d309e44d03e5eee784e92bd
Vincent (Anapher) <anapher@protonmail.com>
2021-02-20 23:13:22 +0100

    finish permission integration tests

--- src/Services/ConferenceManagement/PaderConference.Core.IntegrationTests/Services/PermissionTests.cs
+++ src/Services/ConferenceManagement/PaderConference.Core.IntegrationTests/Services/PermissionTests.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Threading.Tasks;
 using Autofac;
 using Microsoft.Extensions.Options;
+using Moq;
+using Newtonsoft.Json.Linq;
 using PaderConference.Core.Domain.Entities;
 using PaderConference.Core.Extensions;
 using PaderConference.Core.IntegrationTests.Services.Base;
 using PaderConference.Core.Services;
 using PaderConference.Core.Services.ConferenceControl.Notifications;
 using PaderConference.Core.Services.Permissions;
+using PaderConference.Core.Services.Permissions.Gateways;
 using PaderConference.Core.Services.Permissions.Options;
 using PaderConference.Core.Services.Permissions.Requests;
 using PaderConference.Core.Services.Synchronization;
@@ -26,18 +30,20 @@ namespace PaderConference.Core.IntegrationTests.Services
         private readonly SynchronizedObjectId _participantSyncObj =
             SynchronizedParticipantPermissionsProvider.GetObjIdOfParticipant(ParticipantId);
 
-        private static readonly PermissionDescriptor<bool> _testPermission = new("test");
+        private static readonly PermissionDescriptor<bool> TestPermission = new("test");
 
         private static readonly Participant TestParticipant = new(ConferenceId, ParticipantId);
+        private readonly Mock<IPermissionLayerProvider> _permissionLayerProvider = new();
 
         private class TestPermissionValidator : DefinedPermissionValidator
         {
-            public override bool TryGetDescriptor(string permissionKey, out PermissionDescriptor? permissionDescriptor)
+            public override bool TryGetDescriptor(string permissionKey,
+                [NotNullWhen(true)] out PermissionDescriptor? permissionDescriptor)
             {
                 if (base.TryGetDescriptor(permissionKey, out permissionDescriptor)) return true;
-                if (permissionKey == _testPermission.Key)
+                if (permissionKey == TestPermission.Key)
                 {
-                    permissionDescriptor = _testPermission;
+                    permissionDescriptor = TestPermission;
                     return true;
                 }
 
@@ -47,6 +53,8 @@ namespace PaderConference.Core.IntegrationTests.Services
 
         public PermissionTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper)
         {
+            _permissionLayerProvider.Setup(x => x.FetchPermissionsForParticipant(It.IsAny<Participant>()))
+                .ReturnsAsync(Enumerable.Empty<PermissionLayer>());
         }
 
         protected override void ConfigureContainer(ContainerBuilder builder)
@@ -58,6 +66,7 @@ namespace PaderConference.Core.IntegrationTests.Services
                 .AsImplementedInterfaces();
 
             builder.RegisterInstance(new TestPermissionValidator()).AsImplementedInterfaces();
+            builder.RegisterInstance(_permissionLayerProvider.Object).AsImplementedInterfaces();
         }
 
         protected override IEnumerable<Type> FetchServiceTypes()
@@ -91,7 +100,7 @@ namespace PaderConference.Core.IntegrationTests.Services
 
             await Mediator.Publish(new ParticipantInitializedNotification(TestParticipant));
             await Mediator.Publish(new ParticipantJoinedNotification(TestParticipant));
-            var permission = _testPermission.Configure(true);
+            var permission = TestPermission.Configure(true);
 
             // act
             var result = await Mediator.Send(new SetTemporaryPermissionRequest(TestParticipant, permission.Key,
@@ -104,7 +113,7 @@ namespace PaderConference.Core.IntegrationTests.Services
                     _participantSyncObj);
 
             Assert.NotNull(syncObj);
-            Assert.Contains(permission.Key, syncObj.Permissions.Keys);
+            Assert.Contains(permission.Key, syncObj!.Permissions.Keys);
         }
 
         [Fact]
@@ -115,7 +124,7 @@ namespace PaderConference.Core.IntegrationTests.Services
 
             await Mediator.Publish(new ParticipantInitializedNotification(TestParticipant));
             await Mediator.Publish(new ParticipantJoinedNotification(TestParticipant));
-            var permission = _testPermission.Configure(true);
+            var permission = TestPermission.Configure(true);
 
             // act
             var result = await Mediator.Send(new SetTemporaryPermissionRequest(TestParticipant, permission.Key,
@@ -124,10 +133,65 @@ namespace PaderConference.Core.IntegrationTests.Services
             // assert
             Assert.True(result.Success);
 
+            await AssertTestParticipantHasTestPermissionValue(true);
+        }
+
+        [Fact]
+        public async Task UpdateParticipantsPermissions_NewPermissionsAvailable()
+        {
+            // arrange
+            await SetParticipantJoined(TestParticipant);
+
+            await Mediator.Publish(new ParticipantInitializedNotification(TestParticipant));
+            await Mediator.Publish(new ParticipantJoinedNotification(TestParticipant));
+
+            await AssertTestParticipantHasTestPermissionValue(false);
+
+            // act
+            var permissionLayer = new PermissionLayer(0, "test",
+                new Dictionary<string, JValue>(TestPermission.Configure(true).Yield()));
+
+            _permissionLayerProvider.Setup(x => x.FetchPermissionsForParticipant(TestParticipant))
+                .ReturnsAsync(permissionLayer.Yield);
+
+            await Mediator.Send(new UpdateParticipantsPermissionsRequest(TestParticipant.Yield()));
+
+            // assert
+            await AssertTestParticipantHasTestPermissionValue(true);
+        }
+
+        [Fact]
+        public async Task ParticipantLeft_ClearCachedPermissions()
+        {
+            // arrange
+            await SetParticipantJoined(TestParticipant);
+
+            await Mediator.Publish(new ParticipantInitializedNotification(TestParticipant));
+            await Mediator.Publish(new ParticipantJoinedNotification(TestParticipant));
+
+            await Mediator.Send(new SetTemporaryPermissionRequest(TestParticipant, TestPermission.Key,
+                (JValue) JToken.FromObject(true)));
+
+            // act
+            await Mediator.Publish(new ParticipantLeftNotification(TestParticipant, "any"));
+
+            // assert
+            var repo = Container.Resolve<IAggregatedPermissionRepository>();
+            var cachedPermissions = await repo.GetPermissions(TestParticipant);
+            Assert.Empty(cachedPermissions);
+
+            var tempRepo = Container.Resolve<ITemporaryPermissionRepository>();
+            var tempPermissions = await tempRepo.FetchTemporaryPermissions(TestParticipant);
+            Assert.Empty(tempPermissions);
+        }
+
+        private async Task AssertTestParticipantHasTestPermissionValue(bool expected)
+        {
             var participantPermissions = Container.Resolve<IParticipantPermissions>();
-            var stack = await participantPermissions.FetchForParticipant(new Participant(ConferenceId, ParticipantId));
-            var actualValue = await stack.GetPermissionValue(_testPermission);
-            Assert.True(actualValue);
+            var currentPermissions = await participantPermissions.FetchForParticipant(TestParticipant);
+            var actualValue = await currentPermissions.GetPermissionValue(TestPermission);
+
+            Assert.Equal(expected, actualValue);
         }
     }
 }
