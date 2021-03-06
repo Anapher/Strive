c787cf5906d54b203104593ae63d5f4873fd2ff1
Vincent (Anapher) <anapher@protonmail.com>
2021-03-06 21:50:27 +0100

    add participant list integration tests

---
+++ src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/ParticipantsListTests.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using PaderConference.Core.Services.ParticipantsList;
+using PaderConference.Core.Services.Synchronization;
+using PaderConference.IntegrationTests._Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PaderConference.IntegrationTests.Controllers
+{
+    [Collection(IntegrationTestCollection.Definition)]
+    public class ParticipantsListTests : ServiceIntegrationTest
+    {
+        private static readonly SynchronizedObjectId SyncObjId = SynchronizedParticipantsProvider.SynchronizedObjectId;
+
+        public ParticipantsListTests(ITestOutputHelper testOutputHelper, MongoDbFixture mongoDb) : base(
+            testOutputHelper, mongoDb)
+        {
+        }
+
+        [Fact]
+        public async Task Join_AddParticipantToList_UpdateSynchronizedObject()
+        {
+            // arrange
+            var (connection, _) = await ConnectToOpenedConference();
+
+            // assert
+            await connection.SyncObjects.AssertSyncObject<SynchronizedParticipants>(SyncObjId,
+                value => Assert.Contains(value.Participants,
+                    x => x.Key == connection.User.Sub && x.Value.DisplayName == connection.User.Name &&
+                         x.Value.IsModerator == connection.User.IsModerator));
+        }
+
+        [Fact]
+        public async Task Join_AddAnotherParticipant_UpdateSynchronizedObject()
+        {
+            // arrange
+            var (connection, conference) = await ConnectToOpenedConference();
+
+            var olaf = CreateUser();
+
+            // act
+            await ConnectUserToConference(olaf, conference);
+
+            // assert
+            await connection.SyncObjects.AssertSyncObject<SynchronizedParticipants>(SyncObjId,
+                value => Assert.Contains(value.Participants, x => x.Key == olaf.Sub));
+        }
+
+        [Fact]
+        public async Task Join_ParticipantLeaves_UpdateSynchronizedObject()
+        {
+            // arrange
+            var (connection, conference) = await ConnectToOpenedConference();
+
+            var olaf = CreateUser();
+            await ConnectUserToConference(olaf, conference);
+
+            // act
+            await connection.Connection.StopAsync();
+
+            // assert
+            await connection.SyncObjects.AssertSyncObject<SynchronizedParticipants>(SyncObjId,
+                value => Assert.DoesNotContain(value.Participants, x => x.Key == olaf.Sub));
+        }
+    }
+}
