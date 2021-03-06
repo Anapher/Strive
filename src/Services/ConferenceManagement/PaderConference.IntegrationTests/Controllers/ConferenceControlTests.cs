26fc37045cd7c8bf9f0214ee23d697c092060d2e
Vincent (Anapher) <anapher@protonmail.com>
2021-03-06 17:31:48 +0100

    add more integration tests for conference control

--- src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/ConferenceControlTests.cs
+++ src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/ConferenceControlTests.cs
@@ -1,6 +1,10 @@
 ﻿using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.SignalR.Client;
+using PaderConference.Core.Interfaces;
 using PaderConference.Core.Services;
 using PaderConference.Core.Services.ConferenceControl;
+using PaderConference.Hubs;
 using PaderConference.IntegrationTests._Helpers;
 using Xunit;
 using Xunit.Abstractions;
@@ -35,44 +39,44 @@ namespace PaderConference.IntegrationTests.Controllers
             Assert.False(conferenceControlObj.IsOpen);
         }
 
-        //[Fact]
-        //public async Task OpenConference_UserIsModerator_UpdateSynchronizedObject()
-        //{
-        //    // arrange
-        //    var info = await InitializeConferenceAndConnect(true);
+        [Fact]
+        public async Task OpenConference_UserIsModerator_UpdateSynchronizedObject()
+        {
+            // arrange
+            var info = await InitializeConferenceAndConnect(true);
 
-        //    // act
-        //    var result = await OpenConference(info);
+            // act
+            var result = await OpenConference(info);
 
-        //    // assert
-        //    Assert.True(result.Success);
+            // assert
+            Assert.True(result.Success);
 
-        //    var conferenceControlObj =
-        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
-        //            .SynchronizedObjectId);
+            var conferenceControlObj =
+                info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
+                    .SynchronizedObjectId);
 
-        //    Assert.True(conferenceControlObj.IsOpen);
-        //}
+            Assert.True(conferenceControlObj.IsOpen);
+        }
 
-        //[Fact]
-        //public async Task CloseConference_UserIsModerator_UpdateSynchronizedObject()
-        //{
-        //    // arrange
-        //    var info = await InitializeConferenceAndConnect(true);
-        //    await OpenConference(info);
+        [Fact]
+        public async Task CloseConference_UserIsModerator_UpdateSynchronizedObject()
+        {
+            // arrange
+            var info = await InitializeConferenceAndConnect(true);
+            await OpenConference(info);
 
-        //    // act
-        //    var result = await info.Connection.InvokeAsync<SuccessOrError<Unit>>(nameof(CoreHub.CloseConference));
+            // act
+            var result = await info.Connection.InvokeAsync<SuccessOrError<Unit>>(nameof(CoreHub.CloseConference));
 
-        //    // assert
-        //    Assert.True(result.Success);
+            // assert
+            Assert.True(result.Success);
 
-        //    var conferenceControlObj =
-        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
-        //            .SynchronizedObjectId);
+            var conferenceControlObj =
+                info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
+                    .SynchronizedObjectId);
 
-        //    Assert.False(conferenceControlObj.IsOpen);
-        //}
+            Assert.False(conferenceControlObj.IsOpen);
+        }
     }
 }
 
