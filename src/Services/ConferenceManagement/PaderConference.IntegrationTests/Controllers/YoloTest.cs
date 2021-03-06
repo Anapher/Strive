7a3148208b03113d5a1e69b85eb62a4e2574d736
Vincent (Anapher) <anapher@protonmail.com>
2021-03-06 12:43:49 +0100

    i hate my entire life when this fails

---
+++ src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/YoloTest.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace PaderConference.IntegrationTests.Controllers
+{
+    public class YoloTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly CustomWebApplicationFactory _factory;
+
+        public YoloTest(CustomWebApplicationFactory factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public void FooTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
