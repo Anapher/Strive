444ef318daedb22dcfbd47caad000b78b3af31a8
Vincent (Anapher) <anapher@protonmail.com>
2021-03-06 12:41:02 +0100

    Thomas Had Never Seen Such Bullshit Before

--- src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/ConferenceControlTests.cs
+++ src/Services/ConferenceManagement/PaderConference.IntegrationTests/Controllers/ConferenceControlTests.cs
@@ -1,75 +1,75 @@
-﻿using System.Threading.Tasks;
-using PaderConference.IntegrationTests._Helpers;
-using Xunit;
-using Xunit.Abstractions;
+﻿//using System.Threading.Tasks;
+//using PaderConference.IntegrationTests._Helpers;
+//using Xunit;
+//using Xunit.Abstractions;
 
-namespace PaderConference.IntegrationTests.Controllers
-{
-    public class ConferenceControlTests : ServiceIntegrationTest
-    {
-        public ConferenceControlTests(CustomWebApplicationFactory factory, ITestOutputHelper testOutputHelper) : base(
-            factory, testOutputHelper)
-        {
-        }
+//namespace PaderConference.IntegrationTests.Controllers
+//{
+//    public class ConferenceControlTests : ServiceIntegrationTest
+//    {
+//        public ConferenceControlTests(CustomWebApplicationFactory factory, ITestOutputHelper testOutputHelper) : base(
+//            factory, testOutputHelper)
+//        {
+//        }
 
-        [Fact]
-        public async Task OpenConference_UserNotModerator_PermissionDenied()
-        {
-            // arrange
-            //var info = await InitializeConferenceAndConnect();
+//        [Fact]
+//        public async Task OpenConference_UserNotModerator_PermissionDenied()
+//        {
+//            // arrange
+//            //var info = await InitializeConferenceAndConnect();
 
-            // act
-            //var result = await OpenConference(info);
+//            // act
+//            //var result = await OpenConference(info);
 
-            //// assert
-            //Assert.False(result.Success);
-            //AssertErrorCode(ServiceErrorCode.PermissionDenied, result.Error!);
+//            //// assert
+//            //Assert.False(result.Success);
+//            //AssertErrorCode(ServiceErrorCode.PermissionDenied, result.Error!);
 
-            //var conferenceControlObj =
-            //    info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
-            //        .SynchronizedObjectId);
+//            //var conferenceControlObj =
+//            //    info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
+//            //        .SynchronizedObjectId);
 
-            //Assert.False(conferenceControlObj.IsOpen);
-        }
+//            //Assert.False(conferenceControlObj.IsOpen);
+//        }
 
-        //[Fact]
-        //public async Task OpenConference_UserIsModerator_UpdateSynchronizedObject()
-        //{
-        //    // arrange
-        //    var info = await InitializeConferenceAndConnect(true);
+//        //[Fact]
+//        //public async Task OpenConference_UserIsModerator_UpdateSynchronizedObject()
+//        //{
+//        //    // arrange
+//        //    var info = await InitializeConferenceAndConnect(true);
 
-        //    // act
-        //    var result = await OpenConference(info);
+//        //    // act
+//        //    var result = await OpenConference(info);
 
-        //    // assert
-        //    Assert.True(result.Success);
+//        //    // assert
+//        //    Assert.True(result.Success);
 
-        //    var conferenceControlObj =
-        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
-        //            .SynchronizedObjectId);
+//        //    var conferenceControlObj =
+//        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
+//        //            .SynchronizedObjectId);
 
-        //    Assert.True(conferenceControlObj.IsOpen);
-        //}
+//        //    Assert.True(conferenceControlObj.IsOpen);
+//        //}
 
-        //[Fact]
-        //public async Task CloseConference_UserIsModerator_UpdateSynchronizedObject()
-        //{
-        //    // arrange
-        //    var info = await InitializeConferenceAndConnect(true);
-        //    await OpenConference(info);
+//        //[Fact]
+//        //public async Task CloseConference_UserIsModerator_UpdateSynchronizedObject()
+//        //{
+//        //    // arrange
+//        //    var info = await InitializeConferenceAndConnect(true);
+//        //    await OpenConference(info);
 
-        //    // act
-        //    var result = await info.Connection.InvokeAsync<SuccessOrError<Unit>>(nameof(CoreHub.CloseConference));
+//        //    // act
+//        //    var result = await info.Connection.InvokeAsync<SuccessOrError<Unit>>(nameof(CoreHub.CloseConference));
 
-        //    // assert
-        //    Assert.True(result.Success);
+//        //    // assert
+//        //    Assert.True(result.Success);
 
-        //    var conferenceControlObj =
-        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
-        //            .SynchronizedObjectId);
+//        //    var conferenceControlObj =
+//        //        info.SyncObjects.GetSynchronizedObject<SynchronizedConferenceInfo>(SynchronizedConferenceInfoProvider
+//        //            .SynchronizedObjectId);
 
-        //    Assert.False(conferenceControlObj.IsOpen);
-        //}
-    }
-}
+//        //    Assert.False(conferenceControlObj.IsOpen);
+//        //}
+//    }
+//}
 
