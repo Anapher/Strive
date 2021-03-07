586a4719f7c52e0b3fc610c81f77068630b7693d
Vincent (Anapher) <anapher@protonmail.com>
2021-03-07 12:08:12 +0100

    add sync obj integration test

---
+++ src/Services/ConferenceManagement/PaderConference.IntegrationTests/Services/SynchronizedObjectTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using PaderConference.Core.Services.Synchronization;
+using PaderConference.IntegrationTests._Helpers;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace PaderConference.IntegrationTests.Services
+{
+    [Collection(IntegrationTestCollection.Definition)]
+    public class SynchronizedObjectTests : ServiceIntegrationTest
+    {
+        public SynchronizedObjectTests(ITestOutputHelper testOutputHelper, MongoDbFixture mongoDb) : base(
+            testOutputHelper, mongoDb)
+        {
+        }
+
+        [Fact]
+        public async Task Join_ConferenceOpen_ReceiveSubscriptions()
+        {
+            var syncObjId = SynchronizedSubscriptionsProvider.GetObjIdOfParticipant(Moderator.Sub);
+
+            // arrange
+            var (connection, _) = await ConnectToOpenedConference();
+
+            // act
+            var subscriptions = await connection.SyncObjects.WaitForSyncObj<SynchronizedSubscriptions>(syncObjId);
+
+            // assert
+            Assert.NotEmpty(subscriptions.Subscriptions);
+        }
+    }
+}
