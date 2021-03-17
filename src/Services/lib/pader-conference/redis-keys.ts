22f4354580eccadda6092cbea403b4f8f959f08d
Vincent (Anapher) <anapher@protonmail.com>
2021-03-17 21:10:33 +0100

    remove old sfu files

--- src/Services/lib/pader-conference/redis-keys.ts
+++
@@ -1,9 +0,0 @@
-export const participantPermissions = (participantId: string): string => `participantPermissions::${participantId}`;
-
-export const participantToRoom = (conferenceId: string): string => `${conferenceId}::participantToRoom`;
-
-export const newConferences = 'newConferences';
-
-export const conferenceStreams = (conferenceId: string): string => `${conferenceId}::streams`;
-
-export const openConferences = 'openConferences';
