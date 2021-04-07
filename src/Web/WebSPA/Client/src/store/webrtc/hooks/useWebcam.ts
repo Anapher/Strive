470bd742bd56cf25ef5ed7d46b944dc15a786678
Vincent (Anapher) <anapher@protonmail.com>
2021-04-07 10:36:16 +0200

    adjust webcam bitrates

--- src/Web/WebSPA/Client/src/store/webrtc/hooks/useWebcam.ts
+++ src/Web/WebSPA/Client/src/store/webrtc/hooks/useWebcam.ts
@@ -9,9 +9,9 @@ type UseWebcam = UseMediaState & {
 // Each encoding represents a “spatial layer”. Entries in encodings must be ordered from lowest to highest resolution
 // (encodings[0] means “spatial layer 0” while encodings[N-1] means “spatial layer N-1”, being N the number of simulcast streams).
 const CAM_VIDEO_SIMULCAST_ENCODINGS: RtpEncodingParameters[] = [
-   { rid: 's', maxBitrate: 680000, scaleResolutionDownBy: 4 /** scalabilityMode: 'S1T1' (default) */ },
-   { rid: 'm', maxBitrate: 96000, scaleResolutionDownBy: 2 /** scalabilityMode: 'S1T1' (default) */ },
-   { rid: 'h', maxBitrate: 5000000, scaleResolutionDownBy: 1 /** scalabilityMode: 'S1T1' (default) */ },
+   { rid: 's', maxBitrate: 200 * 1000, scaleResolutionDownBy: 4 /** scalabilityMode: 'S1T1' (default) */ },
+   { rid: 'm', maxBitrate: 300 * 1000, scaleResolutionDownBy: 2 /** scalabilityMode: 'S1T1' (default) */ },
+   { rid: 'h', maxBitrate: 3500 * 1000, scaleResolutionDownBy: 1 /** scalabilityMode: 'S1T1' (default) */ },
 ];
 
 export default function useWebcam(loopback = false): UseWebcam {
