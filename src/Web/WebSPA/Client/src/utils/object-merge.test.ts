332e17f5674ff45f6b2a030fb12d6492a601d190
Vincent (Anapher) <anapher@protonmail.com>
2021-04-08 10:37:02 +0200

    fix object-merge

--- src/Web/WebSPA/Client/src/utils/object-merge.test.ts
+++ src/Web/WebSPA/Client/src/utils/object-merge.test.ts
@@ -18,20 +18,38 @@ test('should merge two objects deep', () => {
    expect(target).toEqual({ a: { a: 1, b: 2 } });
 });
 
-test('should overwrite shallow', () => {
+test('should not overwrite shallow', () => {
    const source = { a: 1 };
    const target = { a: 2 };
 
    mergeDeep(target, source);
 
-   expect(target).toEqual({ a: 1 });
+   expect(target).toEqual({ a: 2 });
 });
 
-test('should overwrite deep', () => {
+test('should not overwrite deep', () => {
    const source = { a: { a: 1 } };
    const target = { a: { a: 2 } };
 
    mergeDeep(target, source);
 
-   expect(target).toEqual({ a: { a: 1 } });
+   expect(target).toEqual({ a: { a: 2 } });
+});
+
+test('should not merge lists', () => {
+   const source = { a: ['hello'] };
+   const target = { a: [] };
+
+   mergeDeep(target, source);
+
+   expect(target).toEqual({ a: [] });
+});
+
+test('should complete objects deep', () => {
+   const source = { a: { a: 1, b: 3 } };
+   const target = { a: { a: 2 } };
+
+   mergeDeep(target, source);
+
+   expect(target).toEqual({ a: { a: 2, b: 3 } });
 });
--- src/Web/WebSPA/Client/src/utils/object-merge.ts
+++ src/Web/WebSPA/Client/src/utils/object-merge.ts
@@ -13,16 +13,18 @@ export function isObject(item?: any): boolean {
  * @param ...sources
  */
 export function mergeDeep(target: any, source: any): any {
-   console.log(source);
-
    if (isObject(target) && isObject(source)) {
       for (const key in source) {
          if (isObject(source[key])) {
-            if (!target[key]) Object.assign(target, { [key]: {} });
+            if (!target[key]) {
+               Object.assign(target, { [key]: {} });
+            }
 
             mergeDeep(target[key], source[key]);
          } else {
-            Object.assign(target, { [key]: source[key] });
+            if (target[key] === null || target[key] === undefined) {
+               Object.assign(target, { [key]: source[key] });
+            }
          }
       }
    }
