96e7c8da001fc30ef044a464fd9f545d6eeef5a4
Vincent (Anapher) <anapher@protonmail.com>
2021-04-14 20:43:20 +0200

    add git info to settings

--- src/Web/WebSPA/Client/src/features/settings/components/CommonSettings.tsx
+++ src/Web/WebSPA/Client/src/features/settings/components/CommonSettings.tsx
@@ -1,7 +1,8 @@
-import { FormControl, InputLabel, makeStyles, MenuItem, Select } from '@material-ui/core';
+import { Box, FormControl, InputLabel, Link, makeStyles, MenuItem, Select, Typography } from '@material-ui/core';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { supportedLanguages } from 'src/services/i18n';
+import GitInfo from 'react-git-info/macro';
 
 const useStyles = makeStyles((theme) => ({
    root: {
@@ -13,6 +14,8 @@ const useStyles = makeStyles((theme) => ({
       maxWidth: 240,
    },
 }));
+
+const gitInfo = GitInfo();
 
 export default function CommonSettings() {
    const classes = useStyles();
@@ -41,6 +44,29 @@ export default function CommonSettings() {
                ))}
             </Select>
          </FormControl>
+
+         <Box mt={4}>
+            <Typography variant="subtitle1">{t('conference.settings.common.strive_info')}</Typography>
+            <Typography>{t('conference.settings.common.strive_creator')}</Typography>
+            <Typography>
+               {t('conference.settings.common.strive_commit')}: {gitInfo.commit.shortHash}{' '}
+               <Typography color="textSecondary" component="span">
+                  ({gitInfo.branch} at {gitInfo.commit.date})
+               </Typography>
+            </Typography>
+            <Box display="flex" mt={2}>
+               <Box>
+                  <Link target="_blank" href="https://github.com/Anapher/Strive">
+                     GitHub
+                  </Link>
+               </Box>
+               <Box ml={1}>
+                  <Link target="_blank" href="http://openstrive.org/">
+                     Website
+                  </Link>
+               </Box>
+            </Box>
+         </Box>
       </div>
    );
 }
