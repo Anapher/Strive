4db1322003f91f56e3b04de4d113ada364e2c7f6
Vincent (Anapher) <anapher@protonmail.com>
2021-04-15 13:20:35 +0200

    set git env args in compose.ps1

--- src/compose.ps1
+++ src/compose.ps1
@@ -1,7 +1,13 @@
 
 $arg = $args[0]
 
-$command = 'docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml -f .\docker-compose.traefik.yml ' + $arg
+$env:GITINFO = ((git log -1 --pretty=format:"%d at %ai") | Out-String).Trim()
+$env:GITCOMMIT = ((git rev-parse --short HEAD) | Out-String).Trim()
+
+Write-Host "GITINFO=$env:GITINFO"
+Write-Host "GITCOMMIT=$env:GITCOMMIT"
+
+$command = "docker-compose -f .\docker-compose.yml -f .\docker-compose.override.yml -f .\docker-compose.traefik.yml $arg"
 
 Write-Host $command
-iex $command
+Invoke-Expression $command
