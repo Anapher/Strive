063f58c94e57324f59b6baaa114da9c876cf0272
Vincent (Anapher) <anapher@protonmail.com>
2021-05-28 20:26:00 +0200

    fix(spa): aria support in create conference form for tabs

--- src/Web/WebSPA/Client/src/features/create-conference/components/CreateConferenceForm.tsx
+++ src/Web/WebSPA/Client/src/features/create-conference/components/CreateConferenceForm.tsx
@@ -12,17 +12,16 @@ import {
    useTheme,
    Zoom,
 } from '@material-ui/core';
-import { TabContext, TabPanel } from '@material-ui/lab';
+import AddIcon from '@material-ui/icons/Add';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { mergeDeep } from 'src/utils/object-merge';
+import { wrapForInputRef } from 'src/utils/reat-hook-form-utils';
 import { ConferenceDataForm } from '../form';
 import TabCommon from './TabCommon';
 import TabModerators from './TabModerators';
 import TabPermissions from './TabPermissions';
-import AddIcon from '@material-ui/icons/Add';
-import { wrapForInputRef } from 'src/utils/reat-hook-form-utils';
-import { mergeDeep } from 'src/utils/object-merge';
-import { useTranslation } from 'react-i18next';
 
 const useStyles = makeStyles((theme) => ({
    form: {
@@ -44,6 +43,36 @@ const useStyles = makeStyles((theme) => ({
    },
 }));
 
+interface TabPanelProps {
+   children?: React.ReactNode;
+   index: number;
+   value: number;
+   className: string;
+}
+
+function TabPanel(props: TabPanelProps) {
+   const { children, value, index, ...other } = props;
+
+   return (
+      <div
+         role="tabpanel"
+         hidden={value !== index}
+         id={`create-conference-tabpanel-${index}`}
+         aria-labelledby={`create-conference-tab-${index}`}
+         {...other}
+      >
+         {value === index && children}
+      </div>
+   );
+}
+
+function a11yProps(index: number) {
+   return {
+      id: `create-conference-tab-${index}`,
+      'aria-controls': `create-conference-tabpanel-${index}`,
+   };
+}
+
 type Props = {
    defaultValues: ConferenceDataForm;
    onSubmit: (data: ConferenceDataForm) => void;
@@ -54,9 +83,9 @@ type Props = {
    mode: 'create' | 'patch';
 };
 
-const patchConferenceData = (callback: (data: ConferenceDataForm) => void, defaultValues: ConferenceDataForm) => (
-   data: ConferenceDataForm,
-) => callback(mergeDeep(data, { configuration: defaultValues.configuration }));
+const patchConferenceData =
+   (callback: (data: ConferenceDataForm) => void, defaultValues: ConferenceDataForm) => (data: ConferenceDataForm) =>
+      callback(mergeDeep(data, { configuration: defaultValues.configuration }));
 
 export default function CreateConferenceForm({
    defaultValues,
@@ -75,9 +104,9 @@ export default function CreateConferenceForm({
       defaultValues,
    });
    const { register, formState, handleSubmit } = form;
-   const [currentTab, setCurrentTab] = useState('1');
+   const [currentTab, setCurrentTab] = useState(0);
 
-   const handleChangeTab = (_: unknown, newValue: string) => {
+   const handleChangeTab = (_: unknown, newValue: number) => {
       setCurrentTab(newValue);
    };
 
@@ -105,48 +134,41 @@ export default function CreateConferenceForm({
                   placeholder={t('dialog_create_conference.unnamed_conference')}
                />
             </Box>
-            <TabContext value={currentTab}>
-               <Paper square className={classes.header}>
-                  <Tabs
-                     value={currentTab}
-                     indicatorColor="primary"
-                     textColor="primary"
-                     onChange={handleChangeTab}
-                     aria-label="options tabs"
-                  >
-                     <Tab label={t('common:common')} value="1" />
-                     <Tab label={t('common:moderator_plural')} value="2" />
-                     <Tab label={t('common:permissions')} value="3" />
-                  </Tabs>
-               </Paper>
-               <Box position="relative" flex={1} minHeight={0}>
-                  <TabPanel value="1" className={classes.tabPanel}>
-                     <TabCommon form={form} />
-                  </TabPanel>
-                  <TabPanel value="2" className={classes.tabPanel}>
-                     <TabModerators form={form} conferenceId={conferenceId} />
-                  </TabPanel>
-                  <TabPanel value="3" className={classes.tabPanel}>
-                     <TabPermissions form={form} />
-                  </TabPanel>
-                  <Zoom
-                     in={currentTab === '2'}
-                     timeout={transitionDuration}
-                     style={{
-                        transitionDelay: `${currentTab === '2' ? transitionDuration.exit : 0}ms`,
-                     }}
-                  >
-                     <Fab
-                        aria-label="Add moderator"
-                        className={classes.fab}
-                        color="primary"
-                        onClick={handleAddModerator}
-                     >
-                        <AddIcon />
-                     </Fab>
-                  </Zoom>
-               </Box>
-            </TabContext>
+            <Paper square className={classes.header}>
+               <Tabs
+                  value={currentTab}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  onChange={handleChangeTab}
+                  aria-label="options tabs"
+               >
+                  <Tab label={t('common:common')} {...a11yProps(0)} />
+                  <Tab label={t('common:moderator_plural')} {...a11yProps(1)} />
+                  <Tab label={t('common:permissions')} {...a11yProps(2)} />
+               </Tabs>
+            </Paper>
+            <Box position="relative" flex={1} minHeight={0}>
+               <TabPanel value={currentTab} index={0} className={classes.tabPanel}>
+                  <TabCommon form={form} />
+               </TabPanel>
+               <TabPanel value={currentTab} index={1} className={classes.tabPanel}>
+                  <TabModerators form={form} conferenceId={conferenceId} />
+               </TabPanel>
+               <TabPanel value={currentTab} index={2} className={classes.tabPanel}>
+                  <TabPermissions form={form} />
+               </TabPanel>
+               <Zoom
+                  in={currentTab === 1}
+                  timeout={transitionDuration}
+                  style={{
+                     transitionDelay: `${currentTab === 1 ? transitionDuration.exit : 0}ms`,
+                  }}
+               >
+                  <Fab aria-label="Add moderator" className={classes.fab} color="primary" onClick={handleAddModerator}>
+                     <AddIcon />
+                  </Fab>
+               </Zoom>
+            </Box>
          </Box>
          <DialogActions>
             <Button autoFocus onClick={onClose} color="primary" disabled={isSubmitting}>
