4b0f60114d96dee5048a4ec77f617764fcd697f0
Vincent (Anapher) <anapher@protonmail.com>
2021-05-29 12:08:12 +0200

    fix(spa): add translation for breakout room chip

--- src/Web/WebSPA/Client/src/features/conference/components/appbar/BreakoutRoomChip.tsx
+++ src/Web/WebSPA/Client/src/features/conference/components/appbar/BreakoutRoomChip.tsx
@@ -1,7 +1,8 @@
-import { Chip, Divider, Grid } from '@material-ui/core';
+import { Chip } from '@material-ui/core';
 import { DateTime } from 'luxon';
 import React from 'react';
 import Countdown from 'react-countdown';
+import { useTranslation } from 'react-i18next';
 import CountdownRenderer from 'src/components/CountdownRenderer';
 import { BreakoutRoomsConfig } from 'src/core-hub.types';
 
@@ -12,29 +13,29 @@ type Props = {
 
 export default function BreakoutRoomChip({ state, className }: Props) {
    const deadline = state.deadline ? DateTime.fromISO(state.deadline) : undefined;
+   const { t } = useTranslation();
 
    return (
       <Chip
+         id="appbar-status-chip-breakoutrooms"
          className={className}
          label={
-            <Grid container>
-               {state.description && (
-                  <>
-                     <span>{state.description}</span>
-                     <Divider orientation="vertical" flexItem style={{ marginLeft: 8, marginRight: 8 }} />
-                  </>
-               )}
+            <>
                <span>
-                  Breakout Room phase
+                  {state.description && `${state.description} | `}
+                  {t('conference.appbar.breakout_room_chip')}
                   {deadline && (
                      <span>
                         {' '}
-                        until {deadline.toLocaleString(DateTime.TIME_24_SIMPLE)} (
-                        <Countdown date={deadline.toJSDate()} renderer={CountdownRenderer} />)
+                        {t('conference.appbar.breakout_room_chip_deadline', {
+                           deadline: deadline.toLocaleString(DateTime.TIME_24_SIMPLE),
+                        }) + ' ('}
+                        <Countdown date={deadline.toJSDate()} renderer={CountdownRenderer} />
+                        {')'}
                      </span>
                   )}
                </span>
-            </Grid>
+            </>
          }
          color="primary"
          size="small"
