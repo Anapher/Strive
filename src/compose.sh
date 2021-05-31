81549e24094dac5cef7c5e4c0eb2b77c3a747525
Vincent (Anapher) <anapher@protonmail.com>
2021-05-31 12:18:06 +0200

    test(e2e): another attempt to give cypress git infos

--- src/compose.sh
+++ src/compose.sh
@@ -2,6 +2,13 @@ export GITCOMMIT=$(git rev-parse --short HEAD)
 export GITREF="$(git log -1 --pretty=format:"%D")"
 export GITTIMESTAMP="$(git log -1 --pretty=format:"%ai")"
 
+export COMMIT_INFO_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+export COMMIT_INFO_MESSAGE=$(git show -s --pretty=%B)
+export COMMIT_INFO_EMAIL=$(git show -s --pretty=%ae)
+export COMMIT_INFO_AUTHOR=$(git show -s --pretty=%an)
+export COMMIT_INFO_SHA=$(git show -s --pretty=%H)
+export COMMIT_INFO_REMOTE=$(git config --get remote.origin.url)
+
 echo "GITREF=$GITREF"
 echo "GITCOMMIT=$GITCOMMIT"
 echo "GITTIMESTAMP=$GITTIMESTAMP"
