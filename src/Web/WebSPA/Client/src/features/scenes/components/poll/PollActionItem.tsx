d1455a73109f1cbb7d002ce68e10490cd3342384
Vincent (Anapher) <anapher@protonmail.com>
2021-06-06 19:39:54 +0200

    fix(spa): could not open poll if breakout rooms are open

--- src/Web/WebSPA/Client/src/features/scenes/components/poll/PollActionItem.tsx
+++ src/Web/WebSPA/Client/src/features/scenes/components/poll/PollActionItem.tsx
@@ -2,10 +2,9 @@ import { MenuItem } from '@material-ui/core';
 import PollIcon from '@material-ui/icons/Poll';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setCreationDialogOpen } from 'src/features/poll/reducer';
 import { ActionListItemProps } from 'src/features/scenes/types';
-import { selectIsBreakoutRoomsOpen } from '../../../breakout-rooms/selectors';
 
 export default function PollActionItem({ onClose }: ActionListItemProps) {
    const dispatch = useDispatch();
@@ -16,10 +15,8 @@ export default function PollActionItem({ onClose }: ActionListItemProps) {
       onClose();
    };
 
-   const isOpen = useSelector(selectIsBreakoutRoomsOpen);
-
    return (
-      <MenuItem id="scene-management-actions-poll" onClick={handleOpen} disabled={isOpen}>
+      <MenuItem id="scene-management-actions-poll" onClick={handleOpen}>
          <PollIcon fontSize="small" style={{ marginRight: 16 }} />
          {t('conference.scenes.poll.label')}
       </MenuItem>
